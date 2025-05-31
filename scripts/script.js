08f9e0a1a2351e63ee8d461d8e8e09c8922636dc
VaishnaviKhopade01 <khopadev2004@gmail.com>
2025-05-31 11:52:59 +0530

    settings added

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,21 @@ let inputImage = document.getElementById("main-img-input")
 let imgContainer = document.getElementById("img-container")
 let mainEditableImg = document.getElementById("img-main")
 let imgUploader = document.getElementById("image-uploader")
+let pannelSettingHandler = document.getElementById("pannel-setting-handler")
+let settingMainPannel = document.getElementById("settings")
+let settingPannel = document.getElementById("setting-pannel")
+
+
+//all image control setting
+
+let blurr = document.getElementById("Blur")
+let contrast = document.getElementById("Contrast")
+let grayscale = document.getElementById("Grayscale")
+// let saturation = document.getElementsById("Saturation")
+let invert = document.getElementById("Invert")
+let brightness = document.getElementById("Brightness")
+let opacity = document.getElementById("Opacity")
+let sepia = document.getElementById("Sepia")
 
 //events
 
@@ -18,5 +33,54 @@ inputImage.addEventListener("change",()=>{   //add event on input file
 
      imgUploader.style.display = "none"
      imgContainer.style.display = "block"
+    })
 
+// open setting pannel 
+
+pannelSettingHandler.addEventListener("click",()=>{
+    settingMainPannel.style.display = "block"
+})
+
+settingMainPannel.addEventListener("click",()=>{
+    settingMainPannel.style.display = "none"
+})
+
+settingPannel.addEventListener("click",(e)=>{
+    e.stopPropagation()
+})
+
+//setting events 
+
+blurr.addEventListener("change", () => {
+    mainEditableImg.style.filter = `blur(${blurr.value}px)`
+})
+
+contrast.addEventListener("change", () => {
+    mainEditableImg.style.filter = `contrast(${contrast.value})`
+})
+
+grayscale.addEventListener("change",()=>{
+    mainEditableImg.style.filter = `grayscale(${grayscale.value})`
+})
+
+// saturation.addEventListener("change", () => {
+//     mainEditableImg.style.filter = `saturation(${saturation.value})`
+
+// })
+
+invert.addEventListener("change",()=>{
+    mainEditableImg.style.filter = `invert(${invert.value})`
+})
+
+brightness.addEventListener("change", () => {
+    mainEditableImg.style.filter = `brightness(${brightness.value - 10})`
+})
+
+
+opacity.addEventListener("change",()=>{
+    mainEditableImg.style.filter = `opacity(${opacity.value})`
+})
+
+sepia.addEventListener("change",()=>{
+    mainEditableImg.style.filter = `sepia(${sepia.value})`
 })
